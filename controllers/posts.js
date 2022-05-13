c6f6394f42f188c4a6f8b377dee7b3af484c5fef
callumzhong <callum.zhong@gmail.com>
2022-05-14 00:02:13 +0800

    feat: 拆分 controller 與 service 服務(僅限此異動 model)

---
+++ controllers/posts.js
@@ -0,0 +1,67 @@
+const PostService = require('../service/postService');
+module.exports = {
+	getPagination: async (req, res, next) => {
+		const page = req.query?.page ?? 1;
+		/* #swagger.responses[200] = {
+		  	schema: {"$ref": "#/definitions/PostPage"},
+				description: "取得分頁資料" } */
+		res.status(200).json();
+	},
+	getOne: (req, res, next) => {
+		/* #swagger.responses[200] = {
+		  	schema: {"$ref": "#/definitions/Post"},
+				description: "取得單筆資料" } */
+		res.status(200).json({});
+	},
+	created: (req, res, next) => {
+		/* #swagger.parameters['obj'] = {
+				in:'body',
+				description: '貼文資料',
+				required: true,
+		  	schema: {"$ref": "#/definitions/PostBody"},
+		} */
+
+		/* #swagger.responses[200] = {
+		  	schema: {"$ref": "#/definitions/Post"},
+				description: "新增成功"
+		} */
+		res.status(200).json({});
+	},
+	updated: (req, res, next) => {
+		/* #swagger.parameters['id'] = { description: '貼文 Id' } */
+		/* #swagger.parameters['obj'] = {
+				in:'body',
+				description: '貼文資料',
+				required: true,
+		  	schema: {"$ref": "#/definitions/PostBody"},
+		} */
+
+		/* #swagger.responses[200] = {
+		  	schema: {"$ref": "#/definitions/Post"},
+				description: "更新成功" 
+		}*/
+
+		/* #swagger.responses[404] = {
+				schema:"查無貼文",
+				description: "更新失敗" 
+		}*/
+		res.status(200).json({});
+	},
+	deleteAll: (req, res, next) => {
+		/* #swagger.responses[200] = {
+				description: "刪除成功" 
+		} */
+		res.status(200).json({});
+	},
+
+	deleteOne: (req, res, next) => {
+		/* #swagger.responses[200] = {
+				description: "刪除成功" 
+		} */
+		/* #swagger.responses[404] = {
+				schema:"查無貼文",
+				description: "刪除失敗" 
+		}*/
+		res.status(200).json({});
+	},
+};
---
+++ service/postService.js
@@ -0,0 +1,11 @@
+const Post = require('../models/post');
+
+module.exports = {
+	getPagination: async () => {},
+	getAll: async () => {},
+	getOne: async () => {},
+	created: async () => {},
+	updated: async () => {},
+	deleteAll: async () => {},
+	deleteOne: async () => {},
+};
