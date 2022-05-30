2813908eaebd1042cdf44b6276d39c5241285bfb
callumzhong <callum.zhong@gmail.com>
2022-05-30 17:36:58 +0800

    fix: 檔名大小寫

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -1,30 +1,30 @@
 const bcrypt = require('bcryptjs/dist/bcrypt');
 
-const crypto = require('crypto'); 
-const Users=require('../models/users.model');
-const  {generateSendJWT}  = require('../service/authService');
+const crypto = require('crypto');
+const Users = require('../models/users.model');
+const { generateSendJWT } = require('../service/authService');
 const validator = require('validator');
 const ErrorHandler = require('../service/errorHandler');
 const { Success } = require('../service/appError');
 const emailHandler = require('../service/email/emailHandler');
-const postService = require('../service/post.Service');
+const postService = require('../service/post.service');
 const followService = require('../service/follow.service');
 
-
-const users={
-    //* #swagger.security = [{ "apiKeyAuth": [] }]
-    async CreateUser(req,res,next){
-        /**
-         * #swagger.tags = ['user - 使用者']
-         * #swagger.summary = '註冊使用者'
-         */
-        try{
-            const UserCount=await Users.find().count();
-            if(UserCount===500){
-                return ErrorHandler(new Error('超過會員上限!'), req, res, next);
-            }
-            let { email, password,confirmPassword,name,birthday,gender,photo  } = req.body;
-            /* #swagger.parameters['obj'] = {
+const users = {
+	//* #swagger.security = [{ "apiKeyAuth": [] }]
+	async CreateUser(req, res, next) {
+		/**
+		 * #swagger.tags = ['user - 使用者']
+		 * #swagger.summary = '註冊使用者'
+		 */
+		try {
+			const UserCount = await Users.find().count();
+			if (UserCount === 500) {
+				return ErrorHandler(new Error('超過會員上限!'), req, res, next);
+			}
+			let { email, password, confirmPassword, name, birthday, gender, photo } =
+				req.body;
+			/* #swagger.parameters['obj'] = {
                     in: 'body',
                     description: '資料格式',
                     schema:{
@@ -38,30 +38,29 @@ const users={
                     }
                 }*/
 
-
-            // 加密密碼
-            password = await bcrypt.hash(req.body.password,12);
-            const newUser = await Users.create({
-                email,
-                password,
-                name,
-                birthday,
-                gender,
-                photo
-            });
-            generateSendJWT(newUser,200,res);            
-        }catch(err){
-            return ErrorHandler(err,req,res,next);            
-        }
-    },
-    //登入
-    async singin(req,res,next){
-         /**
-         * #swagger.tags = ['user - 使用者']
-         * #swagger.summary = '使用者登入'
-         */
-        try{
-            /* #swagger.parameters['obj'] = {
+			// 加密密碼
+			password = await bcrypt.hash(req.body.password, 12);
+			const newUser = await Users.create({
+				email,
+				password,
+				name,
+				birthday,
+				gender,
+				photo,
+			});
+			generateSendJWT(newUser, 200, res);
+		} catch (err) {
+			return ErrorHandler(err, req, res, next);
+		}
+	},
+	//登入
+	async singin(req, res, next) {
+		/**
+		 * #swagger.tags = ['user - 使用者']
+		 * #swagger.summary = '使用者登入'
+		 */
+		try {
+			/* #swagger.parameters['obj'] = {
                     in: 'body',
                     description: '資料格式',
                     schema:{
@@ -70,30 +69,29 @@ const users={
                     }
                 }*/
 
-               
-            const {email,password}=req.body;
-            if(!email||!password){
-                return ErrorHandler(new Error("帳號密碼不可為空"),req,res,next);
-            }
-            const user = await Users.findOne({ email,delflag:false }).select('+password');
-            if(!user){
-                return ErrorHandler(new Error("尚未註冊or已經刪除"),req,res,next); 
-            }
-            const auth = await bcrypt.compare(password, user.password);
-            if(!auth){
-                return ErrorHandler(new Error("您的密碼不正確"),req,res,next);                
-            }
-            console.log(user);
-            generateSendJWT(user,200,res);
-            
-        }catch(err){
-            return ErrorHandler(err,req,res,next); 
-        }
-
-    },
-    async delUser(req,res,next){
-         try{
-            /* #swagger.parameters['obj'] = {
+			const { email, password } = req.body;
+			if (!email || !password) {
+				return ErrorHandler(new Error('帳號密碼不可為空'), req, res, next);
+			}
+			const user = await Users.findOne({ email, delflag: false }).select(
+				'+password',
+			);
+			if (!user) {
+				return ErrorHandler(new Error('尚未註冊or已經刪除'), req, res, next);
+			}
+			const auth = await bcrypt.compare(password, user.password);
+			if (!auth) {
+				return ErrorHandler(new Error('您的密碼不正確'), req, res, next);
+			}
+			console.log(user);
+			generateSendJWT(user, 200, res);
+		} catch (err) {
+			return ErrorHandler(err, req, res, next);
+		}
+	},
+	async delUser(req, res, next) {
+		try {
+			/* #swagger.parameters['obj'] = {
 
                     in: 'body',
                     description: '資料格式',
@@ -196,33 +194,31 @@ const users={
                     } 
                     */
 
-                    Success(res,{message:"忘記密碼，發送mail成功!"});                    
-                });
-            }
-       }
-       catch(err){
-           return ErrorHandler(err,req,res,next); 
-       }
-    },
-    async checkUser(req,res,next){
-        try{
-            if(req.user){
-                Success(res,{message:"已經授權!"});        
-            }
-        }
-        catch(err){
-            //return ErrorHandler(err,req,res,next); 
-            return ErrorHandler(new Error("尚未授權"),req,res,next);
-        }
-    },
-    async GetUser(req,res,next){
-        try{
-           const userdata=await Users.findOne({ _id: req.user.id})
-           .populate({
-            path: 'posts',
-            select: 'type content image likes pay tags'
-          });;
-           /* #swagger.responses[200] = {
+						Success(res, { message: '忘記密碼，發送mail成功!' });
+					},
+				);
+			}
+		} catch (err) {
+			return ErrorHandler(err, req, res, next);
+		}
+	},
+	async checkUser(req, res, next) {
+		try {
+			if (req.user) {
+				Success(res, { message: '已經授權!' });
+			}
+		} catch (err) {
+			//return ErrorHandler(err,req,res,next);
+			return ErrorHandler(new Error('尚未授權'), req, res, next);
+		}
+	},
+	async GetUser(req, res, next) {
+		try {
+			const userdata = await Users.findOne({ _id: req.user.id }).populate({
+				path: 'posts',
+				select: 'type content image likes pay tags',
+			});
+			/* #swagger.responses[200] = {
 		  	schema: {
                     "status": true,
                     "data": {
@@ -244,20 +240,19 @@ const users={
                     }
                     },
 				description: "取得個人頁面資料" } */
-           const result={
-               user:userdata,
-               postCounts: await postService.getPostCountbyGroup(req),
-               follows:await followService.getUserFollowCount(req),
-               privateposts: await postService.getPostCountbyGroup(req),
-               likes:0};
-           Success(res,result);
-        }
-        catch(err){
-            //return ErrorHandler(err,req,res,next); 
-            return ErrorHandler(new Error("尚未授權"),req,res,next);
-        }
-    }
-}
-
+			const result = {
+				user: userdata,
+				postCounts: await postService.getPostCountbyGroup(req),
+				follows: await followService.getUserFollowCount(req),
+				privateposts: await postService.getPostCountbyGroup(req),
+				likes: 0,
+			};
+			Success(res, result);
+		} catch (err) {
+			//return ErrorHandler(err,req,res,next);
+			return ErrorHandler(new Error('尚未授權'), req, res, next);
+		}
+	},
+};
 
 module.exports = users;
