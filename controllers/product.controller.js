6ce6a2350d372c7b53b236971599d10dae38e7dc
callumzhong <callum.zhong@gmail.com>
2022-06-02 20:22:33 +0800

    feat: 變更產品結構新增類型票券

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -1,11 +1,12 @@
 const ProductService = require('../service/product.service');
 module.exports = {
 	getAll: async (req, res, next) => {
+		const { query } = req;
 		/* #swagger.responses[200] = {
      		schema: { $ref: '#/definitions/Product' }
     } 
     */
-		const products = await ProductService.getAll();
+		const products = await ProductService.getAll(query.type);
 		res.status(200).json({
 			status: 'success',
 			data: products,
