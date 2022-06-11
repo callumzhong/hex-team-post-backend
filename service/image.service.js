8a76ec69ddfb50e86ec9fb0553ad111145432764
EmptyWu <chunhsing0921@gmail.com>
2022-06-11 15:58:09 +0800

    f/fixuploadFileSize

--- service/image.service.js
+++ service/image.service.js
@@ -5,8 +5,12 @@ const upload = multer({
 		fileSize: 2 * 1024 * 1024,
 	},
 	fileFilter(req, file, cb) {
+		const reqfileSize = parseInt(req.headers["content-length"]);
 		const ext = path.extname(file.originalname).toLowerCase();
-		if (ext !== '.jpg' && ext !== '.png' && ext !== '.jpeg') {
+		if(2 * 1024 * 1024 < reqfileSize){
+			cb(new Error(`小於2M以下檔案。`));
+		}
+		if (ext !== '.jpg' && ext !== '.png' && ext !== '.jpeg' && fileSize <= 1282810) {
 			cb(new Error('檔案格式錯誤，僅限上傳 jpg、jpeg 與 png 格式。'));
 		}
 		cb(null, true);
